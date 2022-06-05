2699e52f4637c12f1c12ffafe2d1ef4eede97c48
vHeemstra <17368112+vHeemstra@users.noreply.github.com>
2022-06-05 11:19:28 +0200

    Create plugin-error.test.js

---
+++ test/plugin-error.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const PluginError = require('../src/plugin-error');
+const util = require('util');
+
+describe('plugin-error.js', () => {
+	const error = new PluginError('SOMEERROR', 'AaAaA');
+
+	test('includes message', () => {
+		expect( error.toString() ).toMatch(/AaaaaError/);
+	});
+
+	test('includes category', () => {
+		expect( error.toString() ).toMatch(/SOMEERROR/);
+	});
+
+	test('method used when console.log() calls .toString()', () => {
+		expect( error.toString() ).toEqual( error[util.inspect.custom]() );
+	});
+
+});
