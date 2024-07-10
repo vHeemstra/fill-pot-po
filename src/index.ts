06e4b3a969850699deffcba2930620318eeccf97
Philip <17368112+vHeemstra@users.noreply.github.com>
2024-07-10 18:56:46 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,26 +18,3 @@ const testOptions = {
 export default fillPotPo;
 export { fillPotPoSync as sync, testOptions, prepareOptions };
 // export { fillPotPo as default, fillPotPoSync as sync, testOptions, prepareOptions };
-
-/**
- * Unfortunately needed shim for fixing the problem with ugly import syntax.
- * Before:
- *   `const fillPotPo = require('fill-pot-po').default`
- * After:
- *   `const fillPotPo = require('fill-pot-po')`
- * See: https://github.com/egoist/tsup/issues/255#issuecomment-784856826
- */
-/* eslint-disable */
-if (typeof module !== 'undefined') {
-  // @ts-ignore
-  fillPotPo.sync = fillPotPoSync;
-  // @ts-ignore
-  fillPotPo.testOptions = testOptions;
-  // @ts-ignore
-  fillPotPo.prepareOptions = prepareOptions;
-  // @ts-ignore
-  module.exports = fillPotPo;
-  // @ts-ignore
-  module.exports.default = fillPotPo;
-}
-/* eslint-enable */
